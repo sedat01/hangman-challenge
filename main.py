c62636c9b0aa1a34e58cea70313fdce3b312fbfd
sedat01 <sedat9404@gmail.com>
2022-10-03 12:15:17 +0200

    Wrote comments

--- main.py
+++ main.py
@@ -4,9 +4,6 @@ This is the main script to execute to run the game.
 '''
 from utils import game
 
-def main():
-    game = game.Hangman()
-    game.start_game()
-    
-if __name__ == 'main':
-    main()
+game = game.Hangman()
+game.start_game()
+
